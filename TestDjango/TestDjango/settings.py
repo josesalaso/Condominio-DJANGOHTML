1574fdafb25a62c8c8c84790f87ce76807c1904e
Jose <82198272+ppsalas@users.noreply.github.com>
2022-05-19 20:39:28 -0400

    todo perfecto

--- TestDjango/TestDjango/settings.py
+++ TestDjango/TestDjango/settings.py
@@ -83,18 +83,14 @@ WSGI_APPLICATION = 'TestDjango.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.oracle',
-#        'NAME': 'xe',
-#        'USER': 'c##pepe2',
-#        'PASSWORD': 'pepe1234',
-#        'HOST': '127.0.0.1',
-#        'PORT': '1521',
-#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'xe',
+        'USER': 'c##pepe2',
+        'PASSWORD': 'pepe1234',
+        'HOST': '127.0.0.1',
+        'PORT': '1521',
+   }
 }
 
 
